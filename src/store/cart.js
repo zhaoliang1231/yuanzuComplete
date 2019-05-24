ab008d0a3e92d285a2f80f595b01b8b72317da6b
silence0425 <1007876231@qq.com>
2019-05-24 18:20:12 +0800

    更改了购物车页面

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,28 +1,29 @@
 
 const state = {
-  cartLists: [{
-      imgs: ['/static/img/100001433_M.ea439af.jpg'],
+  cartLists: [
+    {
+      imgs: ['/src/static/img/100001433_M.ea439af.jpg'],
       goodsId: 8,
       goodsPrice: 999.00,
       goodsName: "黄川的测试提货券6",
-      goodsNum: 1
+      goodsNum: 2
     },
     {
-      imgs:['/static/img/100001433_M.ea439af.jpg'],
+      imgs:['/src/static/img/100001433_M.ea439af.jpg'],
       goodsId:8,
       goodsPrice:999.00,
       goodsName:"黄川的测试提货券6",
       goodsNum:1
     },
     {
-      imgs:['/static/img/100001433_M.ea439af.jpg'],
+      imgs:['/src/static/img/100001433_M.ea439af.jpg'],
       goodsId:8,
       goodsPrice:999.00,
       goodsName:"黄川的测试提货券6",
       goodsNum:1
     },
     {
-      imgs:['/static/img/100001433_M.ea439af.jpg'],
+      imgs:['/src/static/img/100001433_M.ea439af.jpg'],
       goodsId:8,
       goodsPrice:999.00,
       goodsName:"黄川的测试提货券6",
@@ -34,6 +35,10 @@ const state = {
 const mutations = {
   changeCartLists: function (state, lists) {
     state.cartLists = lists
+  },
+  changeListNumber:function (state, goodslist) {
+
+    state.cartLists[goodslist.index].goodsNum = goodslist.num
   }
 }
 
